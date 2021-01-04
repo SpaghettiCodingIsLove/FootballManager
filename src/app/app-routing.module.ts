f767a4ba065d3ef9df292b64c68bb6fed3cd00a5
Adam Wolszleger <43927273+sylfero@users.noreply.github.com>
2021-01-05 00:38:12 +0100

    Dodanie komponentów

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { JuniorsComponent } from './juniors/juniors.component';
 import { MenuComponent } from './menu/menu.component';
+import { SaveComponent } from './save/save.component';
+import { ScheduleComponent } from './schedule/schedule.component';
+import { TablesComponent } from './tables/tables.component';
+import { TeamComponent } from './team/team.component';
+import { TransfersComponent } from './transfers/transfers.component';
 
 const routes: Routes = [
   {
@@ -11,6 +17,30 @@ const routes: Routes = [
   {
     path: 'menu',
     component: MenuComponent
+  },
+  {
+    path: 'juniors',
+    component: JuniorsComponent
+  },
+  {
+    path: 'save',
+    component: SaveComponent
+  },
+  {
+    path: 'schedule',
+    component: ScheduleComponent
+  },
+  {
+    path: 'tables',
+    component: TablesComponent
+  },
+  {
+    path: 'team',
+    component: TeamComponent
+  },
+  {
+    path: 'transfers',
+    component: TransfersComponent
   }
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,12 +10,19 @@ import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-import {MatButtonModule} from '@angular/material/button';
 import { MenuComponent } from './menu/menu.component';
 import { HomeComponent } from './home/home.component';
+import { JuniorsComponent } from './juniors/juniors.component';
+import { SaveComponent } from './save/save.component';
+import { ScheduleComponent } from './schedule/schedule.component';
+import { TablesComponent } from './tables/tables.component';
+import { TeamComponent } from './team/team.component';
+import { TransfersComponent } from './transfers/transfers.component';
+
+import {MatButtonModule} from '@angular/material/button';
 
 @NgModule({
-  declarations: [AppComponent, MenuComponent, HomeComponent],
+  declarations: [AppComponent, MenuComponent, HomeComponent, JuniorsComponent, SaveComponent, ScheduleComponent, TablesComponent, TeamComponent, TransfersComponent],
   entryComponents: [],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, BrowserAnimationsModule, MatButtonModule],
   providers: [
