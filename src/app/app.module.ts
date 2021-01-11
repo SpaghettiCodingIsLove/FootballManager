b077724352ba4422ade28143891c5782b032321f
zima99 <62467321+zima99@users.noreply.github.com>
2021-01-11 17:26:45 +0100

    Front end

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -20,7 +20,16 @@ import { TeamComponent } from './team/team.component';
 import { TransfersComponent } from './transfers/transfers.component';
 
 import {MatButtonModule} from '@angular/material/button';
-import {MatListModule} from '@angular/material/list';
+import { MatListModule } from '@angular/material/list';
+import { MatTableModule } from '@angular/material/table';
+import { MatSelectModule } from '@angular/material/select';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSliderModule } from '@angular/material/slider';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatInputModule  } from '@angular/material/input';
+
+
+
 
 import { IonicStorageModule } from '@ionic/storage';
 import { HttpClientModule } from '@angular/common/http';
@@ -30,7 +39,7 @@ import { SQLite } from '@ionic-native/sqlite/ngx';
 @NgModule({
   declarations: [AppComponent, MenuComponent, HomeComponent, JuniorsComponent, SaveComponent, ScheduleComponent, TablesComponent, TeamComponent, TransfersComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, BrowserAnimationsModule, MatButtonModule, IonicStorageModule.forRoot(), HttpClientModule, MatListModule],
+    imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, MatDialogModule, BrowserAnimationsModule, MatButtonModule, MatInputModule, MatFormFieldModule, MatTableModule,MatSelectModule, MatSliderModule, IonicStorageModule.forRoot(), HttpClientModule, MatListModule],
   providers: [
     StatusBar,
     SplashScreen,
