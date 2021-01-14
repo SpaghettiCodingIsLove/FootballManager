449e14c08fa99d20d0f8c7a36165b1e2e4a486e3
holmat98 <62332336+holmat98@users.noreply.github.com>
2021-01-14 20:17:03 +0100

    tabela ligowa

--- src/app/tables/tables.component.ts
+++ src/app/tables/tables.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { TestBed } from '@angular/core/testing';
 import { NavController } from '@ionic/angular';
 import { DatabaseService } from '../services/database.service';
 
@@ -10,17 +11,53 @@ import { DatabaseService } from '../services/database.service';
 export class TablesComponent implements OnInit {
   players = [];
   player = {};
+  BundesligaTable = [];
+  PremierLeagueTable = [];
+  Bclub = {};
+  PLclub = {};
   constructor(public navCtrl: NavController, private databseProvider: DatabaseService) { 
     this.databseProvider.getDatabaseState().subscribe(rdy => {
       if (rdy) {
         this.loadPlayerData();
+        this.loadBundesligaTableData();
       }
     })
+  }
+
+  leagueChanged(ob){
+    console.log(ob.value);
+    if(ob.value == "Bundesliga")
+    {
+      this.loadBundesligaTableData();
+      document.getElementById("bundesligaTable").style.display = "block";
+      document.getElementById("premierLeagueTable").style.display = "none";
+      console.log(this.BundesligaTable.length);
+    }
+
+    if(ob.value == "Premier League")
+    {
+      this.loadPremierLeagueTableData();
+      document.getElementById("bundesligaTable").style.display = "none";
+      document.getElementById("premierLeagueTable").style.display = "block";
+      console.log(this.PremierLeagueTable.length);
+    }
   }
 
   loadPlayerData() {
     this.databseProvider.getAllPlayers().then(data => {
       this.players = data;
+    });
+  }
+
+  loadBundesligaTableData(){
+    this.databseProvider.getBundesligaTable().then(data =>{
+      this.BundesligaTable = data;
+    });
+  }
+
+  loadPremierLeagueTableData(){
+    this.databseProvider.getPremierLeagueTable().then(data =>{
+      this.PremierLeagueTable = data;
     });
   }
 
