1d6d1c6088733a8c92893c28e529c45804b9b113
Adam Wolszleger <43927273+sylfero@users.noreply.github.com>
2021-01-17 18:39:51 +0100

    Dodanie pluign files, amiany w stylach, dodanie funkcji exit, zmiany w nawigacji

---
+++ src/app/init-team/init-team.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-init-team',
+  templateUrl: './init-team.component.html',
+  styleUrls: ['./init-team.component.scss'],
+})
+export class InitTeamComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
